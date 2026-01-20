f5b9d801b1b58d9f42ded762ea69f47e26619c08
André <andrefragra@gmail.com>
2026-01-20 19:28:37 -0300

    adicionar

--- src/js/yanderes.js
+++
@@ -1,8 +0,0 @@
-import { renderFooter } from "./modules/layout.js";
-
-import { initMenuLateral } from "./modules/menuLateral.js";
-import { initAno } from "./modules/ano.js";
-
-renderFooter();
-initMenuLateral();
-initAno();
